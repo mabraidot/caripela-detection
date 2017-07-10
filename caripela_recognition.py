25889053aeb82eab62b3be74e9599a6514e39595
mabraidot <mabraidot@gmail.com>
2017-07-09 21:43:16 -0300

    Modo entrenamiento elimina las imagenes luego de terminado

--- caripela_recognition.py
+++ caripela_recognition.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys, os
 from time import sleep
 import cv2
@@ -10,9 +11,26 @@ env = dict(environ)
 env['LD_PRELOAD'] = '/usr/lib/arm-linux-gnueabihf/libv4l/v4l2convert.so'
 
 
+windows = True;
+cantidad_fotos = 10
+intervalo = 30 # Intervalo de tiempo para tomar la foto (FPS)
+fotos_tomadas = 0
+tiempo_transcurrido = 0
+humbral_reconocimiento = 35
+
+
+
+
+if windows:
+	camIndex = 0
+else: 
+	camIndex = -1
+
 face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 model = cv2.createLBPHFaceRecognizer()
-model.load('conocidos.xml')
+if os.path.exists('conocidos.xml'):
+	model.load('conocidos.xml')
+
 names = {}
 with open('conocidos.csv','r') as f:
 	for line in f:
@@ -21,23 +39,21 @@ with open('conocidos.csv','r') as f:
 
 #print names
 
-cantidad_fotos = 10
-intervalo = 1000 # Intervalo de tiempo para tomar la foto
-fotos_tomadas = 0
 
 def knownFace(image):
+	global humbral_reconocimiento
+	
 	w,h = image.shape
-	if not image is None and (w > 0 and h > 0):
+	if not image is None and (w > 0 and h > 0) and len(names) > 0:
 		prediction = model.predict(image)
-		print prediction
-		if prediction[1] < 100 and names[int(prediction[0])]:
+		if prediction[1] < humbral_reconocimiento and names[int(prediction[0])]:
 			return '%s - %s' % (names[int(prediction[0])], str(prediction[1]))
 	return False
 
 
 def extract_features(image):
 
-	global fotos_tomadas
+	global fotos_tomadas, tiempo_transcurrido, intervalo, cantidad_fotos
 	
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(
@@ -50,14 +66,14 @@ def extract_features(image):
 
 	for (x, y, w, h) in faces:
 		# Solo tomaremos las caras reconocidas que cerca de la camara
-		if h > 300:
+		if h > 250:
 			knownName = knownFace(gray[y-25:y+h+25, x-25:x+w+25])
 			if not knownName:
-				if fotos_tomadas < cantidad_fotos:
+				if (tiempo_transcurrido % intervalo) == 0 and fotos_tomadas < cantidad_fotos:
 					cv2.imwrite('Caras/cara_desconocida_'+str(fotos_tomadas)+'.jpg', gray[y-25:y+h+25, x-25:x+w+25])
-				
-				text = str("Desconocido")
-				fotos_tomadas += 1
+					fotos_tomadas += 1
+				text = str("Desconocido - Foto "+str(fotos_tomadas))
+				tiempo_transcurrido += 1
 			else:
 				text = str(knownName)
 			
@@ -80,7 +96,7 @@ def inicio():
 
 inicio()
 
-cam = WebcamVideoStream(src=-1).start()
+cam = WebcamVideoStream(src=camIndex).start()
 sleep(1)
 print '\nAl finalizar presione (q: Salir del programa).'
 while cam.stream.isOpened():
--- caripela_training.py
+++ caripela_training.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys, os
 from time import sleep
 import cv2
@@ -24,37 +25,61 @@ def entrenar():
 
 	
 	nombre = raw_input('\nIngrese el nombre de la persona y presione <Enter>: ')	
-	BASE_PATH = "Caras"
+	BASE_PATH = "Caras/"
 	SEPARATOR=";"
 	names[id] = nombre
 	label = id
+	
+	# Creamos el directorio donde se moveran las imágenes, 
+	# el nombre del directorio es el id de la persona
+	"""directory = BASE_PATH+"/"+str(id)
+	if not os.path.exists(directory):
+		try:
+			os.makedirs(directory)
+		except OSError as e:
+			if e.errno != errno.EEXIST:
+				raise
+	"""	
+	
 	for dirname, dirnames, filenames in os.walk(BASE_PATH):
 		#for filename in os.listdir(dirname):
-		for filename in filenames:
-			abs_path = "%s/%s" % (dirname, filename)
+		for filename in filenames:	
+			abs_path = os.path.join(dirname, filename)
 			#print "%s%s%d" % (abs_path, SEPARATOR, label)
-			images.append(cv2.imread(abs_path, 0))
-			labels.append(int(label))
-
-	
+			if os.path.exists(abs_path):
+				images.append(cv2.imread(abs_path, 0))
+				labels.append(int(label))
+				#os.rename(abs_path, directory+"/"+filename)
+				os.remove(abs_path)
+				
 	(images, labels) = [np.array(lis) for lis in [images, labels]]
-	model = cv2.createLBPHFaceRecognizer()
-	modelFile = 'conocidos.xml'
-	if os.path.exists(modelFile):
-		model.load(modelFile)
-		model.update(images, labels)
-		print 'actualizando...'
+	if len(images) > 0:
+		model = cv2.createLBPHFaceRecognizer()
+		modelFile = 'conocidos.xml'
+		if os.path.exists(modelFile):
+			model.load(modelFile)
+			model.update(images, labels)
+			print 'actualizando...'
+		else:
+			model.train(images, labels)
+			print 'actualizando...'
+		model.save(modelFile)
+		f = open('conocidos.csv', 'a+')
+		f.write(str(id)+', "'+nombre+'"\n')
+		f.close()
+		
+		# Luego de entrenar al algoritmo, eliminamos las imágenes
+		for dirname, dirnames, filenames in os.walk(BASE_PATH):
+			for filename in filenames:	
+				abs_path = os.path.join(dirname, filename)
+				os.remove(abs_path)
+				
 	else:
-		model.train(images, labels)
-		print 'actualizando...'
-	model.save(modelFile)
-	f = open('conocidos.csv', 'a+')
-	f.write(str(id)+', "'+nombre+'"\n')
-	f.close()
+		print u"No hay imágenes disponibles para realizar el entrenamiento."
 	exit()
 
 
-def inicio():
+"""def inicio():
 	opcion = raw_input('\nPresione (t: entrenar): ')
 	if opcion == 't':
 		entrenar()
@@ -64,4 +89,5 @@ def inicio():
 
 
 inicio()
-
+"""
+entrenar()
