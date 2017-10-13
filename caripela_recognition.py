f951d49470aef427a9007be79e30d3e25550162f
mabraidot <mabraidot@gmail.com>
2017-10-12 23:23:59 -0300

    Refactoring de versión

--- caripela_recognition.py
+++ caripela_recognition.py
@@ -43,14 +43,14 @@ cascada = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 # Si existe, cargamos nuestro xml entrenado para identificar caras
 modelo = cv2.LBPHFaceRecognizer_create()
 if os.path.exists('conocidos.xml'):
-	modelo.read('conocidos.xml')
+    modelo.read('conocidos.xml')
 
 # Inicializamos un arreglo con los nombres de las personas conocidas
 nombres = {}
 with open('conocidos.csv','r') as f:
-	for renglon in f:
-		id,nombre = renglon.split(',')
-		nombres[int(id)] = str(nombre).rstrip('\n').strip().strip('"')
+    for renglon in f:
+        id,nombre = renglon.split(',')
+        nombres[int(id)] = str(nombre).rstrip('\n').strip().strip('"')
 
 
 """
@@ -58,20 +58,20 @@ Función que recibe una imágen con un rostro capturado, Intenta identificarlo
 con datos del entrenamiento y devuelve su nombre si tiene éxito o False si no.
 """
 def esUnaCaraConocida(imagen):
-	global umbral_reconocimiento
-	
-	"""
-	Si la imágen existe, intentamos identificarla usando el algoritmo 
-	predict que tiene opencv, si la predicción está dentro del umbral, 
-	la damos por buena y retornamos el nombre de la persona identificada.
+    global umbral_reconocimiento
+    
+    """
+    Si la imágen existe, intentamos identificarla usando el algoritmo 
+    predict que tiene opencv, si la predicción está dentro del umbral, 
+    la damos por buena y retornamos el nombre de la persona identificada.
     """
     w, h = imagen.shape
     if not imagen is None and (w > 0 and h > 0) and len(nombres) > 0:
-		prediccion = modelo.predict(imagen)
-		if prediccion[1] < umbral_reconocimiento and nombres[int(prediccion[0])]:
-			#return '%s - %s' % (nombres[int(prediccion[0])], str(prediccion[1]))
-			return '%s' % (nombres[int(prediccion[0])])
-	return False
+        prediccion = modelo.predict(imagen)
+        if prediccion[1] < umbral_reconocimiento and nombres[int(prediccion[0])]:
+            #return '%s - %s' % (nombres[int(prediccion[0])], str(prediccion[1]))
+            return '%s' % (nombres[int(prediccion[0])])
+    return False
 
 
 """
@@ -80,90 +80,90 @@ Si hay rostros, los recuadra e intenta identificarlos usando el algoritmo
 pre-entrenado, si es una cara conocida, imprime el nombre de la persona en el recuadro.
 """
 def buscarCaras(imagen):
-	
-	global fotos_tomadas, tiempo_transcurrido, intervalo, cantidad_fotos
-	global margen_marco, umbral_desconocidos, tolerancia_desconocidos, nombreConocido
-	# Pasamos la imágen a escala de grises, el algoritmo de reconocimiento lo requiere así
-	grices = imagen.copy()
-	grices = cv2.cvtColor(grices, cv2.COLOR_BGR2GRAY)
-	# Ecualizamos la imágen para normalizar la iluminación, ya que el algoritmo de 
-	# reconocimiento es muy sensible a variaciones de luces y sombras
-	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-	grices = clahe.apply(grices)
-	
-	# Buscamos caras en la imágen en escala de grises
-	caras = cascada.detectMultiScale(
-		grices,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30),
-		flags = cv2.CASCADE_SCALE_IMAGE
-	)
-	
-	# Por cada cara encontrada vamos a dibujar un recuadro y tratar de identificarlas
-	for (x, y, w, h) in caras:
-		# Solo tomaremos las caras reconocidas que estén cerca de la camara, 
-		# cuadro mayor a 250 píxeles
-		if h > 250:
-			# Buscamos la cara entre nuestras caras previamente identificadas
-			nombreCaraConocida = esUnaCaraConocida(grices[y+margen_marco:y+h-margen_marco, x+margen_marco:x+w-margen_marco])
-			# Inicializamos el nombre a mostrar por defecto en el recuadro			
-			texto = str("Desconocido")
-			# Si la cara es conocida reseteamos el umbral de tolerancia a caras desconocidas
-			# y guardamos el nombre para mostrarlo luego en el recuadro
-			if nombreCaraConocida:
-				nombreConocido = nombreCaraConocida
-				tolerancia_desconocidos = umbral_desconocidos
-			
-			# Si la cara no es conocida, empezar a descontar de la tolerancia
-			# para evitar los falsos negativos y continuar mostrando el nombre anterior
-			if not nombreCaraConocida:
-				tolerancia_desconocidos -= 1
-				# Si ya llegamos al umbral de tolerancia, mostrar nombre desconocido y tomar foto
-				if tolerancia_desconocidos <= 0:
-					# Si no conocemos la cara, tomamos una cantidad parametrizable de fotos cada cierta
-					# cantidad de cuadros y las guardamos en un directorio para realizar el entrenamiento
-					if (tiempo_transcurrido % intervalo) == 0 and fotos_tomadas < cantidad_fotos:
-						cv2.imwrite('Caras/cara_desconocida_'+str(fotos_tomadas)+'.jpg', 
-								grices[y+margen_marco:y+h-margen_marco, 
-								x+margen_marco:x+w-margen_marco])
-						fotos_tomadas += 1
-					texto = str("Desconocido - Foto "+str(fotos_tomadas))
-					nombreConocido = False
-					tiempo_transcurrido += 1
+    
+    global fotos_tomadas, tiempo_transcurrido, intervalo, cantidad_fotos
+    global margen_marco, umbral_desconocidos, tolerancia_desconocidos, nombreConocido
+    # Pasamos la imágen a escala de grises, el algoritmo de reconocimiento lo requiere así
+    grices = imagen.copy()
+    grices = cv2.cvtColor(grices, cv2.COLOR_BGR2GRAY)
+    # Ecualizamos la imágen para normalizar la iluminación, ya que el algoritmo de 
+    # reconocimiento es muy sensible a variaciones de luces y sombras
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    grices = clahe.apply(grices)
+    
+    # Buscamos caras en la imágen en escala de grises
+    caras = cascada.detectMultiScale(
+        grices,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    # Por cada cara encontrada vamos a dibujar un recuadro y tratar de identificarlas
+    for (x, y, w, h) in caras:
+        # Solo tomaremos las caras reconocidas que estén cerca de la camara, 
+        # cuadro mayor a 250 píxeles
+        if h > 250:
+            # Buscamos la cara entre nuestras caras previamente identificadas
+            nombreCaraConocida = esUnaCaraConocida(grices[y+margen_marco:y+h-margen_marco, x+margen_marco:x+w-margen_marco])
+            # Inicializamos el nombre a mostrar por defecto en el recuadro			
+            texto = str("Desconocido")
+            # Si la cara es conocida reseteamos el umbral de tolerancia a caras desconocidas
+            # y guardamos el nombre para mostrarlo luego en el recuadro
+            if nombreCaraConocida:
+                nombreConocido = nombreCaraConocida
+                tolerancia_desconocidos = umbral_desconocidos
+            
+            # Si la cara no es conocida, empezar a descontar de la tolerancia
+            # para evitar los falsos negativos y continuar mostrando el nombre anterior
+            if not nombreCaraConocida:
+                tolerancia_desconocidos -= 1
+                # Si ya llegamos al umbral de tolerancia, mostrar nombre desconocido y tomar foto
+                if tolerancia_desconocidos <= 0:
+                    # Si no conocemos la cara, tomamos una cantidad parametrizable de fotos cada cierta
+                    # cantidad de cuadros y las guardamos en un directorio para realizar el entrenamiento
+                    if (tiempo_transcurrido % intervalo) == 0 and fotos_tomadas < cantidad_fotos:
+                        cv2.imwrite('Caras/cara_desconocida_'+str(fotos_tomadas)+'.jpg', 
+                                grices[y+margen_marco:y+h-margen_marco, 
+                                x+margen_marco:x+w-margen_marco])
+                        fotos_tomadas += 1
+                    texto = str("Desconocido - Foto "+str(fotos_tomadas))
+                    nombreConocido = False
+                    tiempo_transcurrido += 1
 
-			if nombreConocido:
-				# Si es un rostro conocido, tomamos el nombre para mostrar en el recuadro
-				texto = str(nombreConocido)
-			
-			# Dibujamos el recuadro sobre la cara y colocamos la etiqueta con el nombre conocido 
-			# o una indicación de que no es un rostro conocido
-			cv2.rectangle(imagen, (x+margen_marco, y+margen_marco), (x+w-margen_marco, y+h-margen_marco), (0, 255, 0), 2)
-			wText, h = cv2.getTextSize(texto, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
-			cv2.rectangle(imagen, (x+margen_marco, y+margen_marco), (x+margen_marco+wText[0]+10, y+margen_marco+20), (0, 255, 0), cv2.cv.CV_FILLED)
-			cv2.putText(imagen, texto, (x+margen_marco+5, y+margen_marco+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),2)
+            if nombreConocido:
+                # Si es un rostro conocido, tomamos el nombre para mostrar en el recuadro
+                texto = str(nombreConocido)
+            
+            # Dibujamos el recuadro sobre la cara y colocamos la etiqueta con el nombre conocido 
+            # o una indicación de que no es un rostro conocido
+            cv2.rectangle(imagen, (x+margen_marco, y+margen_marco), (x+w-margen_marco, y+h-margen_marco), (0, 255, 0), 2)
+            wText, h = cv2.getTextSize(texto, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+            cv2.rectangle(imagen, (x+margen_marco, y+margen_marco), (x+margen_marco+wText[0]+10, y+margen_marco+20), (0, 255, 0), cv2.cv.CV_FILLED)
+            cv2.putText(imagen, texto, (x+margen_marco+5, y+margen_marco+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),2)
 
-	# Mostramos cada cuadro en la ventana de video
-	cv2.imshow('Video', imagen)
+    # Mostramos cada cuadro en la ventana de video
+    cv2.imshow('Video', imagen)
 
-	return
+    return
 
 
 """
 Función que presenta el menú en pantalla para iniciar el reconocimiento
 """
 def inicio():
-	opcion = input('\nParate frente a la cámara y presioná (r: reconococer rostro), \nAl finalizar presioná (q: Salir del programa):')
-	"""
-	Si el usuario presionó la letra r, salimos de la función para que el programa 
-	continúe y entre en el bucle (while) principal. Si es otra letra, la función
-	sigue llamándose a sí misma para que continúe el menú en pantalla
-	"""
-	if opcion == 'r':
-		return
-	else:
-		inicio()
-	return
+    opcion = input('\nParate frente a la cámara y presioná (r: reconococer rostro), \nAl finalizar presioná (q: Salir del programa):')
+    """
+    Si el usuario presionó la letra r, salimos de la función para que el programa 
+    continúe y entre en el bucle (while) principal. Si es otra letra, la función
+    sigue llamándose a sí misma para que continúe el menú en pantalla
+    """
+    if opcion == 'r':
+        return
+    else:
+        inicio()
+    return
 
 """
 Lo primero que se debe hacer al iniciar el programa es llamar a inicio() para 
@@ -182,13 +182,13 @@ Bucle principal. Si pudimos capturar la cámara, procesamos cada cuadro en
 búsqueda de rostros
 """
 while camara.stream.isOpened():
-	# Capturamos un cuadro del video
-	cuadro = camara.read()
-	# Procesamos el cuadro para ver si hay rostros
-	buscarCaras(cuadro.copy())
-	# Si el usuario presiona la letra de salir, abortamos el bucle
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    # Capturamos un cuadro del video
+    cuadro = camara.read()
+    # Procesamos el cuadro para ver si hay rostros
+    buscarCaras(cuadro.copy())
+    # Si el usuario presiona la letra de salir, abortamos el bucle
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # Al salir, liberar la cámara y cerrar la ventana de video
 camara.stop()
--- caripela_training.py
+++ caripela_training.py
@@ -16,50 +16,50 @@ Función que genera el menú de opciones que guía al usuario para realizar el e
 Genera una lista con los nombres conocidos por el modelo y la opción de crear una entrada 
 con una cara nueva.
 """
+opciones = {}
 def menu():
 
     global opciones
-	
-    opciones = {}
+
     (nombres, id) = ({}, 0)
     etiqueta = -1
-	# Abrimos la lista de nombres conocidos y generamos el menú con el nombre y ID de la persona
+    # Abrimos la lista de nombres conocidos y generamos el menú con el nombre y ID de la persona
     with open('conocidos.csv','r') as csv_nombres:
-		for renglon in csv_nombres:
-			etiqueta,nombre = renglon.split(',')
-			nombre = str.replace(nombre, "\"", "")
-			nombre = str.strip(nombre)
-			opciones[int(etiqueta)] = nombre
-	
-	# Agregamos una opción para cargar el nombre de una cara desconocida
+        for renglon in csv_nombres:
+            etiqueta,nombre = renglon.split(',')
+            nombre = str.replace(nombre, "\"", "")
+            nombre = str.strip(nombre)
+            opciones[int(etiqueta)] = nombre
+    
+    # Agregamos una opción para cargar el nombre de una cara desconocida
     opciones['n'] = "Nuevo rostro"
-	
-	# Preparamos el texo del menú
+    
+    # Preparamos el texo del menú
     texto_opciones = "\n\nSi deseás actualizar el modelo de una persona que ya existía, seleccioná la opción con su nombre.\nSi es una persona desconocida, presioná n para entrenar el modelo: \n\n"
-    for id, texto in opciones.iteritems():
-		texto_opciones = texto_opciones + "(" + str(id) + ") " + texto + "\n"
-	
-	# Mostramos el menú en pantalla y esperamos que el usuario elija una opción
+    for id, texto in opciones.items():
+        texto_opciones = texto_opciones + "(" + str(id) + ") " + texto + "\n"
+    
+    # Mostramos el menú en pantalla y esperamos que el usuario elija una opción
     opcion = input(texto_opciones)
-	# Si la opción elegida es salir, salimos
+    # Si la opción elegida es salir, salimos
     if opcion == 'q':
-		exit()
-	# Si eligió nueva cara, usamos el próximo ID disponible y le pedimos al usuario
-	# que ingrese el nombre de la nueva cara
+        exit()
+    # Si eligió nueva cara, usamos el próximo ID disponible y le pedimos al usuario
+    # que ingrese el nombre de la nueva cara
     elif opcion == 'n':
-		nombre = input('\nIngresá el nombre de la persona y presioná <Enter>: ')
-		id = int(etiqueta) + 1
-		return id, nombre
-	# Si desea actualizar una cara ya conocida con nuevas fotos, tomamos el nombre
-	# y ID conocidos
+        nombre = input('\nIngresá el nombre de la persona y presioná <Enter>: ')
+        id = int(etiqueta) + 1
+        return id, nombre
+    # Si desea actualizar una cara ya conocida con nuevas fotos, tomamos el nombre
+    # y ID conocidos
     elif int(opcion) in opciones:
-		nombre = opciones[int(opcion)]
-		id = int(opcion)
-		return id, nombre
-	# Si no eligió alguna de las anteriores, mostramos mensaje de error y cerramos el programa
+        nombre = opciones[int(opcion)]
+        id = int(opcion)
+        return id, nombre
+    # Si no eligió alguna de las anteriores, mostramos mensaje de error y cerramos el programa
     else:
-		print(u"Opción no válida.")
-		exit()
+        print(u"Opción no válida.")
+        exit()
 
 
 
@@ -69,68 +69,68 @@ Si hay fotos para reconocer, las usa para entrenar al algoritmo de reconocimient
 archivo xml de caras conocidas
 """
 def entrenar():
-	
-	global opciones
-	
-	# Inicializamos algunas variables
-	(imagenes, etiquetas, nombres, id) = ([], [], {}, 0)
-	BASE_PATH = "Caras/"
-	SEPARATOR=";"
-	
-	"""
-	Tomamos las fotos dentro de la carpeta definida, guardamos sus datos en un arreglo, 
-	y luego las borramos del directorio para liberar espacio.
-	"""
-	for carpeta, subcarpetas, fotos in os.walk(BASE_PATH):
-		for foto in fotos:	
-			ruta = os.path.join(carpeta, foto)
-			if os.path.exists(ruta):
-				imagenes.append(cv2.imread(ruta, 0))
-				os.remove(ruta)
-	
-	
-	# Si hay imágenes en el directorio, iniciamos el entrenamiento
-	if len(imagenes) > 0:
-	
-		# Mostramos el menú y esperamos la opción elegida por el usuario
-		(id, nombre) = menu()
-		# Por cada foto encontrada en el directorio, generamos una entrada en el arreglo de las etiquetas
-		for etiqueta in imagenes:
-			etiquetas.append(int(id))
-		
-		# Convertimos las imágenes a un array numpy, que es lo que el algoritmo de reconocimiento necesita
-		(imagenes, etiquetas) = [np.array(lista) for lista in [imagenes, etiquetas]]
-		
-		
-		# Inicializamos el modelo
-		modelo = cv2.LBPHFaceRecognizer_create()
-		archivo_modelo = 'conocidos.xml'
-		# Si ya existía el archivo xml de reconocimiento, lo actualizamos
-		if os.path.exists(archivo_modelo):
-			modelo.read(archivo_modelo)
-			modelo.update(imagenes, etiquetas)
-			print(u'Actualizando el modelo...')
-		# Si no existe el archivo xml de reconocimiento, lo creamos
-		else:
-			modelo.train(imagenes, etiquetas)
-			print(u'Creando un nuevo modelo...')
-		
-		# Guardamos los cambios en el modelo
-		modelo.write(archivo_modelo)
-		# Si entrenamos una nueva cara, agregamos su nombre y ID a la lista de personas conocidas
-		if int(id) not in opciones:
-			csv_nombres = open('conocidos.csv', 'a+')
-			csv_nombres.write(str(id)+', "'+nombre+'"\n')
-			csv_nombres.close()
-			print(u'Entrenando la nueva cara de ' + nombre)
-		else:
-			print(u'Actualizando la cara de ' + nombre)
-		
-		
-	# Si no hay imágenes en el directorio, mostramos el error y cerramos el programa
-	else:
-		print(u"\n\nNo hay imágenes disponibles para realizar el entrenamiento.\n\n")
-	exit()
+    
+    global opciones
+    
+    # Inicializamos algunas variables
+    (imagenes, etiquetas, nombres, id) = ([], [], {}, 0)
+    BASE_PATH = "Caras/"
+    SEPARATOR=";"
+    
+    """
+    Tomamos las fotos dentro de la carpeta definida, guardamos sus datos en un arreglo, 
+    y luego las borramos del directorio para liberar espacio.
+    """
+    for carpeta, subcarpetas, fotos in os.walk(BASE_PATH):
+        for foto in fotos:	
+            ruta = os.path.join(carpeta, foto)
+            if os.path.exists(ruta):
+                imagenes.append(cv2.imread(ruta, 0))
+                os.remove(ruta)
+    
+    
+    # Si hay imágenes en el directorio, iniciamos el entrenamiento
+    if len(imagenes) > 0:
+    
+        # Mostramos el menú y esperamos la opción elegida por el usuario
+        (id, nombre) = menu()
+        # Por cada foto encontrada en el directorio, generamos una entrada en el arreglo de las etiquetas
+        for etiqueta in imagenes:
+            etiquetas.append(int(id))
+        
+        # Convertimos las imágenes a un array numpy, que es lo que el algoritmo de reconocimiento necesita
+        (imagenes, etiquetas) = [np.array(lista) for lista in [imagenes, etiquetas]]
+        
+        
+        # Inicializamos el modelo
+        modelo = cv2.LBPHFaceRecognizer_create()
+        archivo_modelo = 'conocidos.xml'
+        # Si ya existía el archivo xml de reconocimiento, lo actualizamos
+        if os.path.exists(archivo_modelo):
+            modelo.read(archivo_modelo)
+            modelo.update(imagenes, etiquetas)
+            print(u'Actualizando el modelo...')
+        # Si no existe el archivo xml de reconocimiento, lo creamos
+        else:
+            modelo.train(imagenes, etiquetas)
+            print(u'Creando un nuevo modelo...')
+        
+        # Guardamos los cambios en el modelo
+        modelo.write(archivo_modelo)
+        # Si entrenamos una nueva cara, agregamos su nombre y ID a la lista de personas conocidas
+        if int(id) not in opciones:
+            csv_nombres = open('conocidos.csv', 'a+')
+            csv_nombres.write(str(id)+', "'+nombre+'"\n')
+            csv_nombres.close()
+            print(u'Entrenando la nueva cara de ' + nombre)
+        else:
+            print(u'Actualizando la cara de ' + nombre)
+        
+        
+    # Si no hay imágenes en el directorio, mostramos el error y cerramos el programa
+    else:
+        print(u"\n\nNo hay imágenes disponibles para realizar el entrenamiento.\n\n")
+    exit()
 
 # Llamada a la función de entrenamiento
 entrenar()
